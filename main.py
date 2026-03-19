e66ec34ce3ef498de9bbb7201ef3154f79b26eeb
hauuto <thanhhau670@gmail.com>
2026-03-19 23:43:19 +0700

    feat: student dashboard, team, match, history

--- main.py
+++ main.py
@@ -26,6 +26,22 @@ async def login_page(request: Request):
 async def register_page(request: Request):
     return templates.TemplateResponse("register_student.html", {"request": request})
 
+@app.get("/student/dashboard", response_class=HTMLResponse)
+async def student_dashboard(request: Request):
+    return templates.TemplateResponse("student/dashboard.html", {"request": request})
+
+@app.get("/student/team", response_class=HTMLResponse)
+async def student_team(request: Request):
+    return templates.TemplateResponse("student/team.html", {"request": request})
+
+@app.get("/student/match", response_class=HTMLResponse)
+async def student_match(request: Request):
+    return templates.TemplateResponse("student/match.html", {"request": request})
+
+@app.get("/student/history", response_class=HTMLResponse)
+async def student_history(request: Request):
+    return templates.TemplateResponse("student/history.html", {"request": request})
+
 @app.get("/admin/login", response_class=HTMLResponse)
 async def admin_login_page(request: Request):
     return templates.TemplateResponse("login_admin.html", {"request": request})
